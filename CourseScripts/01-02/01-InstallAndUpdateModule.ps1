3308055fcf764616ac1fa4f92bb76bd775a7fd40
jpomfret <jpomfret7@gmail.com>
2022-07-07 15:14:07 +0100

    add 01_02 file

---
+++ CourseScripts/01-02/01-InstallAndUpdateModule.ps1
@@ -0,0 +1,19 @@
+# Installing and updating the dbatools module
+
+# Find the dbatools on the PowerShell Gallery
+Find-Module dbatools
+
+# Find modules on the PowerShell Gallery using a pattern
+Find-Module dba*
+
+# Install dbatools for just your user
+Install-Module dbatools
+
+# Install dbatools for all users on your machine
+Install-Module dbatools -Scope AllUsers
+
+# Download the dbatools module and save it into a folder
+Save-Module dbatools -Path c:\temp\dbatools
+
+# Where do modules get saved?
+$env:PSModulePath.split(';')
