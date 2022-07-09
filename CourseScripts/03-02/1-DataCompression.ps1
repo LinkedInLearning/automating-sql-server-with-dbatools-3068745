68390134e573c01a8e637725afbcb7a37b371909
jpomfret <jpomfret7@gmail.com>
2022-07-09 17:41:52 +0100

    add header

---
+++ CourseScripts/03-02/1-DataCompression.ps1
@@ -0,0 +1,7 @@
+<#
+-------------------------------------
+Automating SQL Server with dbatools
+-------------------------------------
+03-02 - Discover and apply recommended data compression levels
+-------------------------------------
+#>
