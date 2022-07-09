14509fef1efd34f89171783afe8e8278f251e934
jpomfret <jpomfret7@gmail.com>
2022-07-09 17:42:24 +0100

    add header

---
+++ CourseScripts/04-01/1-MigrateSQLInstance.ps1
@@ -0,0 +1,7 @@
+<#
+-------------------------------------
+Automating SQL Server with dbatools
+-------------------------------------
+04-01 - Migrate whole instances with one line of code
+-------------------------------------
+#>
