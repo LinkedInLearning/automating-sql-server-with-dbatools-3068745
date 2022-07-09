5e429e810cc58e8755b39b63703f4370fdc18404
jpomfret <jpomfret7@gmail.com>
2022-07-09 16:09:59 +0000

    add 01-04 script

---
+++ CourseScripts/01-04/1-GetHelp.ps1
@@ -0,0 +1,16 @@
+<#
+-------------------------------------
+Automating SQL Server with dbatools
+-------------------------------------
+01_04 - Running your first command
+-------------------------------------
+#>
+
+## Get help on how to use a command
+Get-Help Test-DbaDbCompression
+Get-Help Test-DbaDbCompression -ShowWindow # good for windows!
+Get-Help Test-DbaDbCompression -Online
+Get-Help Test-DbaDbCompression -Examples
+
+# Using one of the examples from the Test-DbaDbCompression help
+Test-DbaDbCompression -SqlInstance $dbatools1
