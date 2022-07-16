d244ad21b56012acc14745910c3128f64011ebc0
jpomfret <jpomfret7@gmail.com>
2022-07-16 14:40:03 +0000

    script updates

--- CourseScripts/02-01/1-GetDatabaseStatus.ps1
+++ CourseScripts/02-01/1-GetDatabaseStatus.ps1
@@ -6,22 +6,36 @@ Automating SQL Server with dbatools
 -------------------------------------
 #>
 
-# Find a command that matches the pattern *database*status*
+# Find a command that matches the pattern of *database*status*
 Find-DbaCommand -Pattern *database*status*
 
 # Review the help for that command
 Get-Help Get-DbaDbState -Full
 
 # Run Get-DbaDbState
-Get-DbaDbState -SqlInstance $dbatools1 | Select
+Get-DbaDbState -SqlInstance $dbatools1
+
+# Run Get-DbaDbState with selected columns
+Get-DbaDbState -SqlInstance $dbatools1 | Select-Object SqlInstance, DatabaseName, Status, RW, Access | Format-Table 
+
+# Find a command that matches the pattern of *database*status*
+Find-DbaCommand -Pattern *database*status*
 
 ## Get databases
 $datatbaseSplat = @{
-    SqlInstance   = $dbatools1
-    ExcludeSystem = $true
-    OutVariable   = "dbs"        # OutVariable to also capture this to use later
+    SqlInstance   = $SQLInstances
 }
-Get-DbaDatabase @datatbaseSplat |
-Select-Object Name, Status, RecoveryModel, Owner, Compatibility |
-Format-Table
+Get-DbaDatabase @datatbaseSplat
 
+## Get databases
+$datatbaseSplat = @{
+    SqlInstance   = $SQLInstances
+}
+Get-DbaDatabase @datatbaseSplat | Select-Object -First 1 *
+
+## Get databases - selected properties, and format as a table
+$datatbaseSplat = @{
+    SqlInstance   = $SQLInstances
+}
+Get-DbaDatabase @datatbaseSplat | Select-Object SqlInstance, Name, Status, RecoveryModel, Owner, Compatibility | 
+Format-Table
