9ac29a8b74bc7cce30d4829623dea1e861f149b9
jpomfret <jpomfret7@gmail.com>
2022-07-16 13:36:17 +0000

    add notes to demo

--- CourseScripts/01-04/1-GetHelp.ps1
+++ CourseScripts/01-04/1-GetHelp.ps1
@@ -8,8 +8,14 @@ Automating SQL Server with dbatools
 
 ## Get help on how to use a command
 Get-Help Test-DbaDbCompression
-Get-Help Test-DbaDbCompression -ShowWindow # good for windows!
+
+# Open up a seperate window with the full help - this won't work here since we're running in a container, but good on Windows machines
+Get-Help Test-DbaDbCompression -ShowWindow
+
+# Open up the online help pages
 Get-Help Test-DbaDbCompression -Online
+
+# View examples on how to use the command
 Get-Help Test-DbaDbCompression -Examples
 
 # Using one of the examples from the Test-DbaDbCompression help
