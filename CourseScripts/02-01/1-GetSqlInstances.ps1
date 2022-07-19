7b9fc380ef6a1bab422bf3123f3438283314c27f
jpomfret <jpomfret7@gmail.com>
2022-07-19 08:54:03 +0000

    add new 02-01

---
+++ CourseScripts/02-01/1-GetSqlInstances.ps1
@@ -0,0 +1,8 @@
+<#
+-------------------------------------
+Automating SQL Server with dbatools
+-------------------------------------
+02-01 - Get a list of SQL Servers in your environment
+-------------------------------------
+#>
+
