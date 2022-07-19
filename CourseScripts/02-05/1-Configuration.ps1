3324d63aa413d795615ef8ef64e5ffba7311d42a
jpomfret <jpomfret7@gmail.com>
2022-07-19 08:45:19 +0000

    update branch

--- CourseScripts/02-05/1-Configuration.ps1
+++ CourseScripts/02-05/1-Configuration.ps1
@@ -2,6 +2,6 @@
 -------------------------------------
 Automating SQL Server with dbatools
 -------------------------------------
-02-05 - Find running SQL Agent jobs
+02-05 - Review SQL Server Configuration Settings
 -------------------------------------
 #>
