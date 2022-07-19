c01dd538231133f1be8bb76020504c8346a9b3b7
jpomfret <jpomfret7@gmail.com>
2022-07-19 08:43:49 +0000

    add script

--- CourseScripts/02-02/1-CheckDatabaseBackups.ps1
+++ CourseScripts/02-02/1-CheckDatabaseBackups.ps1
@@ -5,3 +5,12 @@ Automating SQL Server with dbatools
 02-02 - RunningCheck database last backup times
 -------------------------------------
 #>
+
+# Find commands that match the pattern backup
+Find-DbaCommand backup
+
+# View example uses
+Get-Help Get-DbaDbBackupHistory -Examples
+
+# Get backup history for the dbatools1 instance
+Get-DbaDbBackupHistory -SqlInstance $dbatools1
