2dfb4c20a14d5f10ccbed72fb3405c3ad9f546b2
jpomfret <jpomfret7@gmail.com>
2022-08-07 15:22:20 +0000

    add script

--- CourseScripts/04-01/1-MigrateSQLInstance.ps1
+++ CourseScripts/04-01/1-MigrateSQLInstance.ps1
@@ -5,3 +5,15 @@ Automating SQL Server with dbatools
 04-01 - Migrate whole instances with one line of code
 -------------------------------------
 #>
+
+# Check the help docs
+Get-Help Start-DbaMigration
+
+# Migrate everything from dbatools1 to dbatools2
+$migrateSplat = @{
+    Source = 'dbatools1'
+    Destination = 'dbatools2'
+    BackupRestore = $true
+    SharedPath = '/shared'
+}
+Start-DbaMigration @migrateSplat
