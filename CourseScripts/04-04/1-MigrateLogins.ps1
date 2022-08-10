712b80172d4eb43a0377fbe44ffcf1c2ffda3182
jpomfret <jpomfret7@gmail.com>
2022-08-10 18:44:05 +0000

    start of scripts

--- CourseScripts/04-04/1-MigrateLogins.ps1
+++ CourseScripts/04-04/1-MigrateLogins.ps1
@@ -5,3 +5,12 @@ Automating SQL Server with dbatools
 04-04 - Migrating logins
 -------------------------------------
 #>
+
+# View the logins on dbatools1
+Get-DbaLogin -SqlInstance dbatools1 |
+Select-Object SqlInstance, Name, LoginType
+
+# View the database users for pubs
+Get-DbaDbUser -SqlInstance dbatools1 -Database Pubs -ExcludeSystemUser |
+Select-Object SqlInstance, Database, Name, Login
+
