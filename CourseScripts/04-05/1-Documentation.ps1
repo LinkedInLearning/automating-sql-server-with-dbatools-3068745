9fc38c0dc8d4c2df1eb2e1be1c1eca3fd8900b6f
jpomfret <jpomfret7@gmail.com>
2022-08-12 17:09:17 +0000

    documentation!

--- CourseScripts/04-05/1-Documentation.ps1
+++ CourseScripts/04-05/1-Documentation.ps1
@@ -5,3 +5,11 @@ Automating SQL Server with dbatools
 04-05 - Exporting documentation
 -------------------------------------
 #>
+
+
+$instanceSplat = @{
+    SqlInstance   = $SQLInstances
+    Path          = '.\Export\'
+    Exclude       = 'ReplicationSettings','CentralManagementServer'
+}
+Export-DbaInstance @instanceSplat
